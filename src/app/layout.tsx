b59fd3d8159c315bd0c99bc5f95e38d1fc2a7994
JOESTOE001 <joost3108@hotmail.com>
2026-04-29 09:56:40 +0200

    google tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,22 +29,17 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="nl">
-      <Script id="google-tag-manager" strategy="beforeInteractive">
-        {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-P5MZJ6KK');`}
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-GEVLNC5HKL"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'G-GEVLNC5HKL');`}
       </Script>
       <body className={`${dmSans.variable} ${playfair.variable} font-sans antialiased`}>
-        <noscript>
-          <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-P5MZJ6KK"
-            height="0"
-            width="0"
-            style={{ display: 'none', visibility: 'hidden' }}
-          />
-        </noscript>
         {children}
         <LandeigenaarPopup />
         <Analytics />
